436ad4add785da22935e6833cb5dfb4ad9aa6095
faizansafwan <faizansafwan075@gmail.com>
2025-05-17 14:49:47 +0530

    middleware for protect routes

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,18 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res, next) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password'); // Attach full user
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
